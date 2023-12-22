fa3c77361669aff4a1ac13aca49faa6ca209dd20
Parseval <igwee3333@gmail.com>
2023-12-22 22:20:46 +0100

    chore: removed comments

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -3,17 +3,16 @@ pragma solidity ^0.8.9;
 
 contract LogisticsDocumentation {
     struct Document {
-        string documentHash; // Hash of the document
-        address uploader;    // Address of the uploader
-        uint256 timestamp;   // Timestamp of when the document was uploaded
+        string documentHash; 
+        address uploader;   
+        uint256 timestamp;   
     }
 
-    mapping(uint256 => Document) public documents; // Mapping of document IDs to Document struct
-    uint256 public documentCount; // Counter for the number of documents
+    mapping(uint256 => Document) public documents; 
+    uint256 public documentCount; 
 
     event DocumentAdded(uint256 indexed docId, string documentHash, address indexed uploader, uint256 timestamp);
-
-    // Function to add a document to the blockchain
+    
     function addDocument(string memory _documentHash) public {
         require(bytes(_documentHash).length > 0, "Document hash cannot be empty");
         documentCount++;
@@ -21,14 +20,12 @@ contract LogisticsDocumentation {
         emit DocumentAdded(documentCount, _documentHash, msg.sender, block.timestamp);
     }
 
-    // Function to retrieve a document's details by ID
     function getDocument(uint256 _docId) public view returns (string memory, address, uint256) {
         require(_docId > 0 && _docId <= documentCount, "Invalid document ID");
         Document memory doc = documents[_docId];
         return (doc.documentHash, doc.uploader, doc.timestamp);
     }
 
-    // Function to get the total number of documents stored
     function getTotalDocuments() public view returns (uint256) {
         return documentCount;
     }
